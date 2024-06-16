70ca9bbab79f0f9eabf733528645d066ddb5ca37
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-06-17 03:18:48 +0500

    https://github.com/junaidsheikh0002/Responsive_On_Eid_Ul_Azha_Websites_Made_By_Junaid_Ul_Haque_Sheikh_17_06_2024_Night_0001/upload
    https://github.com/junaidsheikh0002/Responsive_On_Eid_Ul_Azha_Websites_Made_By_Junaid_Ul_Haque_Sheikh_17_06_2024_Night_0001/upload
    https://github.com/junaidsheikh0002/Responsive_On_Eid_Ul_Azha_Websites_Made_By_Junaid_Ul_Haque_Sheikh_17_06_2024_Night_0001/upload
    https://github.com/junaidsheikh0002/Responsive_On_Eid_Ul_Azha_Websites_Made_By_Junaid_Ul_Haque_Sheikh_17_06_2024_Night_0001/upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("celebrateButton").addEventListener("click", function() {
+    alert("Happy Eid ul Azha!");
+});
---
+++ style.cpp
@@ -0,0 +1,71 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f9f9f9;
+}
+
+header {
+    background-color: #28a745;
+    color: white;
+    padding: 1em 0;
+    text-align: center;
+}
+
+header h1 {
+    margin: 0;
+}
+
+nav ul {
+    list-style: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin: 0 1em;
+}
+
+nav ul li a {
+    color: white;
+    text-decoration: none;
+}
+
+main {
+    padding: 2em;
+}
+
+section {
+    margin-bottom: 2em;
+}
+
+footer {
+    background-color: #28a745;
+    color: white;
+    text-align: center;
+    padding: 1em 0;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+}
+
+button {
+    background-color: #28a745;
+    color: white;
+    border: none;
+    padding: 1em;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: #218838;
+}
+
+.responsive-image {
+    width: 100%;
+    height: auto;
+    max-width: 600px;
+    display: block;
+    margin: 1em auto;
+}
